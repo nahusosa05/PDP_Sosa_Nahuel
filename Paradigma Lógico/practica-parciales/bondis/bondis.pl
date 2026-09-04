f5096f98e878ee49119b2a1ddb049d95b6cf0e77
nahusosa05 <nasosa@frba.utn.edu.ar>
2026-09-03 23:57:59 -0300

    practica parcial: bondis 2

--- Paradigma Lógico/practica-parciales/bondis/bondis.pl
+++ Paradigma Lógico/practica-parciales/bondis/bondis.pl
@@ -1,5 +1,7 @@
 % Recorridos en GBA: 
 recorrido(17, gba(sur), mitre). 
+recorrido(17, gba(sur), calle2).
+recorrido(17, gba(sur), calle3).  
 recorrido(24, gba(sur), belgrano). 
 recorrido(10, gba(oeste), 404).
 recorrido(247, gba(sur), onsari). 
@@ -7,7 +9,7 @@ recorrido(60, gba(norte), maipu).
 recorrido(152, gba(norte), olivos). 
  
 % Recorridos en CABA: 
-recorrido(17, caba, santaFe). 
+%recorrido(17, caba, santaFe). 
 recorrido(152, caba, santaFe). 
 recorrido(10, caba, santaFe). 
 recorrido(160, caba, medrano). 
@@ -54,7 +56,7 @@ jurisdiccion(Linea, provincial(Provincia)) :-
 provinciaDeLaLinea(Linea, buenosAires) :-
     recorrido(Linea, gba(_), _).
 
-privinciaDeLaLinea(Linea, caba) :-
+provinciaDeLaLinea(Linea, caba) :-
     recorrido(Linea, caba, _).
 
 cruzaLaGralPaz(Linea) :-
@@ -124,12 +126,134 @@ esCalleTransbordo(Zona, Calle) :-
     forall(recorrido(Linea, Zona, Calle), jurisdiccion(Linea, nacional)).
     
 /*
-    Punto 5: Necesitamos  incorporar  a  la  base  de  conocimientos  cuáles  son  los  beneficios  que  las 
-    personas tienen asociadas a sus tarjetas registradas en el sistema SUBE. Dichos beneficios 
-    pueden ser cualquiera de los siguientes: 
-    - Estudiantil: el boleto tiene un costo fijo de $50. 
-    - Personal de casas particulares: nos interesará registrar para este beneficio cuál es la  zona  en  la 
-            que  se  encuentra  el  domicilio  laboral.  Si  la  línea  que  se  toma  la persona  con este  
-            beneficio  pasa  por  dicha  zona,  se  subsidia  el  valor  total  del boleto, por lo que no tiene costo. 
-    - Jubilado: el boleto cuesta la mitad de su valor. 
+    =======================================
+                   Punto 5.a
+    =======================================
+    Incorporar a la base de conocimientos los beneficios asociados a las 
+    tarjetas SUBE de las personas.
+
+    Tipos de beneficios:
+    - Estudiantil: costo fijo de $50.
+    - Personal de casas particulares: registra la zona del domicilio laboral. 
+      Si la línea pasa por dicha zona, se subsidia el valor total ($0).
+    - Jubilado: el boleto cuesta la mitad de su valor original.
+
+    Representar la información de los beneficios y beneficiarios.
 */
+
+% tiene(Persona, Beneficio).
+% Pepito tiene el beneficio de personal de casas particulares dentro de la zona oeste del GBA.
+tiene(pepito, personalDeCasasParticulares(gba(oeste))).
+
+% Juanita tiene el beneficio del boleto estudiantil.
+tiene(juanita, estudiantil).
+
+% Tito no tiene ningún beneficio. NO SE CODEA
+
+% Marta tiene beneficio de jubilada y también de personal de casas particulares dentro de caba y en zona sur del GBA.
+tiene(marta, jubilada).
+tiene(marta, personalDeCasasParticulares(caba)).
+tiene(marta, personalDeCasasParticulares(gba(sur))).
+
+/*
+    =======================================
+                   Punto 5.b
+    =======================================
+    Saber, para una persona, cuánto le costaría viajar en una línea, considerando que:
+
+    1. Valor normal del boleto (sin considerar beneficios):
+       - $500: si la línea es de jurisdicción nacional.
+       - $350: si es provincial de CABA.
+       - Provincia de Buenos Aires: $25 multiplicado por la cantidad de calles 
+         que tiene en su recorrido, más un plus de $50 si pasa por zonas diferentes 
+         de la provincia.
+
+    2. Beneficios aplicados:
+       - La persona abona el valor correspondiente según sus beneficios.
+       - En caso de contar con múltiples beneficios, abona el monto más bajo 
+         (los descuentos no son acumulativos).
+
+    Ejemplo:
+       Para Marta, el boleto en una línea nacional que pasa por CABA es gratuito ($0) 
+       por el beneficio de casas particulares. En cambio, en una línea provincial de 
+       Buenos Aires que recorre zona norte y oeste únicamente, abona la mitad del 
+       viaje normal: ($50 + $25 * cantidadDeCalles) / 2, por su condición de jubilada.
+*/
+
+/*
+    El valor normal del boleto (o sea, sin considerar beneficios) es de $500 si la 
+    línea es de jurisdicción nacional y de $350 si es provincial de CABA. 
+*/
+
+valorNormal(Linea, 500):-
+    jurisdiccion(Linea, nacional).
+
+valorNormal(Linea, 350):-
+    jurisdiccion(Linea, provincial(caba)).
+
+/*
+    En caso de ser de jurisdicción de la provincia de Buenos Aires, cuesta $25 
+    multiplicado por la cantidad de calles que tiene en su recorrido mas un plus
+    de $50 si pasa por zonas diferentes de la provincia. 
+*/
+
+valorNormal(Linea, Valor) :-
+    jurisdiccion(Linea, provincial(buenosAires)),
+    cantidadCalles(Linea, CantidadCalles), % cantidadCalles(15, CantidadCalles).
+    plusZonasDiferentes(Linea, Plus),
+    Valor is (25 * CantidadCalles) + Plus.
+
+cantidadCalles(Linea, Cantidad) :-
+    findall(Calle, recorrido(Linea, _, Calle), Calles), % 15 [santaFe, medrano, rivadavia, santaFe]
+    list_to_set(Calles, CallesSinRepetidos),            % 15 [santaFe, medrano, rivadavia]
+    length(CallesSinRepetidos, Cantidad).   % length([santaFe, medrano, rivadavia], 3).
+    
+pasaPorZonasDiferentes(Linea) :-
+    recorrido(Linea, gba(Zona1), _),
+    recorrido(Linea, gba(Zona2), _),
+    Zona1 \= Zona2.
+
+plusZonasDiferentes(Linea, 50) :-
+    pasaPorZonasDiferentes(Linea).
+
+plusZonasDiferentes(Linea, 0) :-
+    linea(Linea),
+    not(pasaPorZonasDiferentes(Linea)).
+
+/*
+    La  persona  debería abonar el valor que corresponda dependiendo de los 
+    beneficios  que  tenga.  En  caso  de tener más de un beneficio, el monto a 
+    abonar debería ser el más bajo (los descuentos no son acumulativos). 
+*/
+
+% Si tiene beneficio SUBE.
+calcularCosto(Persona, Linea, Costo) :-
+    tiene(Persona, Beneficio),
+    valorNormal(Linea, ValorNormal),
+    valorConBeneficio(Beneficio, Linea, ValorNormal, Costo).
+
+% No tiene beneficio SUBE.
+calcularCosto(Persona, Linea, ValorNormal) :-
+    persona(Persona),
+    valorNormal(Linea, ValorNormal).
+
+persona(tito).
+persona(pepito).
+persona(marta).
+persona(juanita).
+
+valorConBeneficio(estudiantil, _, _, 50).
+
+valorConBeneficio(jubilada, _, ValorNormal, Costo) :-
+    Costo is ValorNormal / 2.
+
+valorConBeneficio(personalDeCasasParticulares(ZonaLaboral), Linea, _, 0) :-
+    recorrido(Linea, ZonaLaboral, _).
+
+cuantoPaga(Persona, Linea, MontoMinimo) :-
+    calcularCosto(Persona, Linea, MontoMinimo),
+    forall(
+      calcularCosto(Persona, Linea, OtrosMontos),
+      MontoMinimo =< OtrosMontos  
+    ).
+    
